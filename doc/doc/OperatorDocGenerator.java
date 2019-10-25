49050faa345ec2909aa78f943731a4c35d4b5659
wangj_lc <wangj_lc@inspur.com>
2019-10-25 09:21:24 +0800

    Initial Commit

---
+++ doc/doc/OperatorDocGenerator.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright (C) 2001-2019 RapidMiner GmbH
+ */
+package base.operators.doc;
+
+import java.io.PrintWriter;
+
+import base.operators.operator.Operator;
+import com.sun.javadoc.RootDoc;
+
+
+/**
+ * Generates the documentation for operators.
+ * 
+ * @author Simon Fischer
+ */
+public interface OperatorDocGenerator {
+
+	/** Generates the documentation for this operator and writes it to the given writer. */
+	public void generateDoc(Operator operator, RootDoc rootDoc, PrintWriter out);
+
+	/** Generates the header for the group with the given name. The name might be null. */
+	public void beginGroup(String groupName, PrintWriter out);
+
+	/** Generates the footer for the group with the given name. The name might be null. */
+	public void endGroup(String groupName, PrintWriter out);
+}
